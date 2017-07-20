4126b82eddd41d191c2b5eb81675998bd21e3925
William Martin <wgm2111@columbia.edu>
2017-07-20 15:04:47 +0200

    Added a doc string to the ellipsoidal outlier detetion module.

--- ellipsoidal_outlier_detector.py
+++ ellipsoidal_outlier_detector.py
@@ -1,4 +1,27 @@
 
+"""
+# Ellipsoidal outlier detection module
+
+This module can be used to do ellipsoidal outlier detection in Python.  Given a scatter plot of `M` points in `ndim`-dimensions, you can select the points that are farthest from the center.  
+
+** Basic usage **
+
+Import and use the following functions : 
+- get_outliers: Does one ellipsoidal outlier detection step.
+- get_total_partition: Makes a sequence of calls to `get_outliers`, returns lists. 
+- get_filter_index: Helper routine for applying a filter to new data. 
+
+** Details ** 
+
+The `EllipsoidSolver` class is used to actually do the optimization that finds the minimum volume ellipsoid that contains all the specified points.  The optimization depends on the CVXOPT package.  For CVXOPT documentation see the website, cvxopt.org.  Also, the book "Convex Optimization" by [Boyd and Vandenberghe] might be useful.  
+
+** Notes **
+There may be some room to improve performance using sparse arrays.  But it should work fairly well up to ndim = 8 and M = 10,000. [July 2017]
+
+
+"""
+
+
 
 
 # Future
